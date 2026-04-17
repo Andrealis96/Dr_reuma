14a94fe422bcdadc5a0b6cb29625ce82eb31d0ca
Andrealis96 <andrea1996lisbeth@gmail.com>
2026-04-17 14:26:19 -0300

    cambio de carrusel

--- src/components/HeroCarousel.jsx
+++ src/components/HeroCarousel.jsx
@@ -1,60 +1,67 @@
-import Carousel from 'react-bootstrap/Carousel'
+import { useEffect, useState } from "react";
 
 // DESKTOP
-import img1 from '../assets/banner1.webp'
-import img2 from '../assets/banner2.webp'
-import img3 from '../assets/banner3.webp'
-import img4 from '../assets/banner4.webp'
-import img5 from '../assets/banner5.webp'
+import img1 from "../assets/banner1.webp";
+import img2 from "../assets/banner2.webp";
+import img3 from "../assets/banner3.webp";
+import img4 from "../assets/banner4.webp";
+import img5 from "../assets/banner5.webp";
 
-// MOBILE 🔥
-import imgMobile1 from '../assets/bannertelef1.webp'
-import imgMobile2 from '../assets/bannertelef2.webp'
-import imgMobile3 from '../assets/bannertelef3.webp'
-import imgMobile4 from '../assets/bannertelef4.webp'
-import imgMobile5 from '../assets/bannertelef5.webp'
+// MOBILE
+import m1 from "../assets/bannertelef1.webp";
+import m2 from "../assets/bannertelef2.webp";
+import m3 from "../assets/bannertelef3.webp";
+import m4 from "../assets/bannertelef4.webp";
+import m5 from "../assets/bannertelef5.webp";
+
+const desktopImages = [img1, img2, img3, img4, img5];
+const mobileImages = [m1, m2, m3, m4, m5];
 
 function HeroCarousel() {
-    return (
-        <Carousel fade interval={4500} controls indicators className="hero-carousel">
-        
-            <Carousel.Item>
-                <picture>
-                    <source media="(max-width: 768px)" srcSet={imgMobile1} />
-                    <img className="d-block w-100 hero-img" src={img1} alt="Atención en reumatología" loading="eager"  style={{ minHeight: "200px" }}/>
-                </picture>
-            </Carousel.Item>
+  const [index, setIndex] = useState(0);
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
 
-            <Carousel.Item>
-                <picture>
-                    <source media="(max-width: 768px)" srcSet={imgMobile2} />
-                    <img className="d-block w-100 hero-img" src={img2} alt="Diagnóstico preciso"  style={{ minHeight: "200px" }}/>
-                </picture>
-            </Carousel.Item>
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
 
-            <Carousel.Item>
-                <picture>
-                    <source media="(max-width: 768px)" srcSet={imgMobile3} />
-                    <img className="d-block w-100 hero-img" src={img3} alt="Tratamientos personalizados"  style={{ minHeight: "200px" }}/>
-                </picture>
-            </Carousel.Item>
+    window.addEventListener("resize", handleResize);
 
-            <Carousel.Item>
-                <picture>
-                    <source media="(max-width: 768px)" srcSet={imgMobile4} />
-                    <img className="d-block w-100 hero-img" src={img4} alt="Tratamientos personalizados"  style={{ minHeight: "200px" }}/>
-                </picture>
-            </Carousel.Item>
+    const interval = setInterval(() => {
+      setIndex((prev) =>
+        prev === desktopImages.length - 1 ? 0 : prev + 1
+      );
+    }, 4500);
 
-            <Carousel.Item>
-                <picture>
-                    <source media="(max-width: 768px)" srcSet={imgMobile5} />
-                    <img className="d-block w-100 hero-img" src={img5} alt="Tratamientos personalizados"  style={{ minHeight: "200px" }}/>
-                </picture>
-            </Carousel.Item>
+    return () => {
+      window.removeEventListener("resize", handleResize);
+      clearInterval(interval);
+    };
+  }, []);
 
-        </Carousel>
-    )
+  const images = isMobile ? mobileImages : desktopImages;
+
+  return (
+    <section className="hero">
+      <img
+        src={images[index]}
+        alt="Banner"
+        className="hero-img"
+      />
+
+      {/* indicadores */}
+      <div className="hero-dots">
+        {images.map((_, i) => (
+          <span
+            key={i}
+            className={i === index ? "dot active" : "dot"}
+            onClick={() => setIndex(i)}
+          />
+        ))}
+      </div>
+    </section>
+  );
 }
 
-export default HeroCarousel
+export default HeroCarousel;
