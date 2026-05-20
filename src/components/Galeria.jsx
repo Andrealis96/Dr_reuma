d1399d14abaa679f83846ded8da780a59c2968e5
Andrealis96 <andrea1996lisbeth@gmail.com>
2026-05-20 15:13:07 -0300

    cambios colores

--- src/components/Galeria.jsx
+++ src/components/Galeria.jsx
@@ -1,5 +1,14 @@
-
 import "../styles/App.css";
+
+import {
+  FaChevronLeft,
+  FaChevronRight,
+  FaPlayCircle,
+  FaImages
+} from "react-icons/fa";
+
+import { useRef } from "react";
+
 import esclerodermiaImg from "../assets/esclerodermiaa.webp";
 import rodillaImg from "../assets/rodilla.webp";
 import dermatomiositisImg from "../assets/dermatomiositiss.webp";
@@ -8,101 +17,229 @@ import protesisImg from "../assets/protesis.webp";
 import gotaImg from "../assets/gotaa.webp";
 import osteoporosisImg from "../assets/osteoporosiss.webp";
 import fibromialgiaImg from "../assets/fibromialgiaa.webp";
+import paciente1 from "../assets/paciente1.webp"
+import paciente2 from "../assets/paciente2.webp"
+import paciente3 from "../assets/paciente3.webp"
+import paciente4 from "../assets/paciente4.webp"
+import paciente5 from "../assets/paciente5.webp"
+
 import gotavideo from "../assets/gotavideo.mp4";
 import esclerodermiavideo from "../assets/esclerodermiavideo.mp4";
 import artritisvideo from "../assets/artritisreumavideo.mp4";
 
 function Galeria() {
-  const items = [
+
+  const videosRef = useRef(null);
+  const pacientesRef = useRef(null);
+  const casosRef = useRef(null);
+
+  const scroll = (ref, direction) => {
+    if (!ref.current) return;
+
+    ref.current.scrollBy({
+      left: direction === "left" ? -350 : 350,
+      behavior: "smooth"
+    });
+  };
+
+  const videos = [
     {
-      type: "video",
       src: gotavideo,
-      title: "ARTITIS REMAUTOIDE",
+      title: "ARTROSIS SEVERA EN PIES"
     },
     {
-      type: "video",
       src: esclerodermiavideo,
-      title: "ESCLERODERMIA",
+      title: "ESCLERODERMIA"
     },
     {
-      type: "video",
       src: artritisvideo,
-      title: "ARTRITIS REUMATOIDE",
+      title: "ARTRITIS REUMATOIDE"
+    }
+  ];
+
+  const imagenespacientes = [
+    {
+      src: paciente1,
+      title: "EXAMEN FÍSICO"
     },
     {
-      type: "image",
-      src: esclerodermiaImg,
-      title: "ESCLERODERMIA",
+      src: paciente2,
+      title: "DOLOR EN ARTICULACIONES"
     },
     {
-      type: "image",
+      src: paciente3,
+      title: "DESCUBRIENDO MOLESTIAS"
+    },
+    {
+      src: paciente4,
+      title: "EXAMEN DE RUTINA"
+    },
+    {
+      src: paciente5,
+      title: "TOMANDO HISTORIA CLÍNICA"
+    },
+    {
       src: rodillaImg,
-      title: "DOLOR DE RODILLA",
-    },
+      title: "DOLOR DE RODILLA"
+    }
+  
+];
+      const imagenescasos = [
     {
-      type: "image",
+      src: esclerodermiaImg,
+      title: "ESCLERODERMIA"
+    },
+  
+    {
       src: dermatomiositisImg,
-      title: "DERMATOMIOSITIS",
+      title: "DERMATOMIOSITIS"
     },
     {
-      type: "image",
       src: lupusImg,
-      title: "LUPUS",
+      title: "LUPUS"
     },
     {
-      type: "image",
-      src: protesisImg,
-      title: "ARTOSIS AVANZADA DE RODILLA QUE LLEGA A PRÓTESIS",
-    },
-    {
-      type: "image",
       src: gotaImg,
-      title: "PIE DE GOTA",
+      title: "PIE DE GOTA"
     },
     {
-      type: "image",
-      src: osteoporosisImg,
-      title: "MÚLTIPLES FRACTURAS POR OSTEOPOROSIS",
-    },
-    {
-      type: "image",
       src: fibromialgiaImg,
-      title: "FIBROMIALGIA TRATABLE",
+      title: "FIBROMIALGIA"
     },
+    {
+      src: protesisImg,
+      title: "ARTROSIS AVANZADA"
+    },
+    {
+      src: osteoporosisImg,
+      title: "OSTEOPOROSIS"
+    }
   ];
 
   return (
     <section className="galeria" id="galeriaa">
+      <div className="container py-1">  
       <h2 className="subtitle-general mb-5">
         <span className="subtitle-celeste">GALERÍA DE </span>
         <span className="subtitle-negro">CASOS</span>
-        </h2>
-      <div className="galeria-grid">
-        {items.map((item, index) => (
-            <div className="galeria-card" key={index}>
-            
-            {item.type === "video" ? (
-              <video
-                  className="galeria-media"
-                  src={item.src}
-                  controls
-                  preload="metadata"
-                  muted
-                />
-            ) : (
-                <img
-                className="galeria-media "
-                src={item.src}
-                alt={item.title}
-                />
-            )}
+      </h2>
 
-            <p className="galeria-caption">{item.title}</p>
+      {/* VIDEOS */}
+      <div className="galeria-section">
 
+        <div className="galeria-header">
+          <h3>
+            <FaPlayCircle /> VIDEOS DE CASOS
+          </h3>
+
+          <div className="galeria-buttons">
+            <button onClick={() => scroll(videosRef, "left")}>
+              <FaChevronLeft />
+            </button>
+
+            <button onClick={() => scroll(videosRef, "right")}>
+              <FaChevronRight />
+            </button>
           </div>
-        ))}
+        </div>
+
+        <div className="galeria-slider" ref={videosRef}>
+
+          {videos.map((video, index) => (
+            <div className="video-card" key={index}>
+
+              <video
+                src={video.src}
+                controls
+                preload="metadata"
+                muted
+              />
+
+              <p>{video.title}</p>
+
+            </div>
+          ))}
+
+        </div>
       </div>
-       
+
+
+
+      {/* IMÁGENES */}
+      <div className="galeria-section">
+
+        <div className="galeria-header">
+          <h3>
+            <FaImages /> IMÁGENES CON PACIENTES
+          </h3>
+
+          <div className="galeria-buttons">
+            <button onClick={() => scroll(pacientesRef, "left")}>
+              <FaChevronLeft />
+            </button>
+
+            <button onClick={() => scroll(pacientesRef, "right")}>
+              <FaChevronRight />
+            </button>
+          </div>
+        </div>
+
+        <div className="galeria-slider" ref={pacientesRef}>
+
+          {imagenespacientes.map((img, index) => (
+            <div className="image-card" key={index}>
+
+              <img
+                src={img.src}
+                alt={img.title}
+              />
+
+              <p>{img.title}</p>
+
+            </div>
+          ))}
+
+        </div>
+      </div>
+    
+     {/* IMÁGENES */}
+      <div className="galeria-section">
+
+        <div className="galeria-header">
+          <h3>
+            <FaImages /> IMÁGENES DE CASO DE ENFERMEDADES
+          </h3>
+
+          <div className="galeria-buttons">
+            <button onClick={() => scroll(casosRef, "left")}>
+              <FaChevronLeft />
+            </button>
+
+            <button onClick={() => scroll(casosRef, "right")}>
+              <FaChevronRight />
+            </button>
+          </div>
+        </div>
+
+        <div className="galeria-slider" ref={casosRef}>
+
+          {imagenescasos.map((img, index) => (
+            <div className="image-card" key={index}>
+
+              <img
+                src={img.src}
+                alt={img.title}
+              />
+
+              <p>{img.title}</p>
+
+            </div>
+          ))}
+
+        </div>
+      </div>
+    
+      </div>
     </section>
   );
 }
